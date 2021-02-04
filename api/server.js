c7b365aeea2f8b12aa072103da0f4622be3d5d73
amichalo421 <andre.michalowski@gmail.com>
2021-02-04 18:53:37 -0500

    Import back to server

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require("express"); // import express
+const hubsRouter = require('./routers/hubs-router.js'); //Step 2A: Import Router
 const server = express(); // instantiate server
 server.use(express.json()); // configure server
+
+server.use('/api/hubs', hubsRouter); //Step 2B: Create root URL for router
 
 server.get('/', (req, res) => {
 	res.status(200).json({ serverMessage: 'Test endpoint' });

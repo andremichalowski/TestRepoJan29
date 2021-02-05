cc8d5822717810ad3bb7c85971ab24be9fc0f44d
amichalo421 <andre.michalowski@gmail.com>
2021-02-04 19:04:08 -0500

    Create Middleware

--- api/server.js
+++ api/server.js
@@ -3,7 +3,15 @@ const hubsRouter = require('./routers/hubs-router.js'); //Step 2A: Import Router
 const server = express(); // instantiate server
 server.use(express.json()); // configure server
 
-server.use('/api/hubs', hubsRouter); //Step 2B: Create root URL for router
+function passCheck(req, res, next) { // 1. Create Middleware
+	if (req.headers.password === "mellon") {
+			next();
+	} else {
+			res.status(401).json({ errorMessage: "Inccorect password" });
+	}
+}
+
+server.use('/api/hubs', passCheck, hubsRouter); // 2. Add to route instantiation
 
 server.get('/', (req, res) => {
 	res.status(200).json({ serverMessage: 'Test endpoint' });
